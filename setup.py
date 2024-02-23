28220f9dbf72cadf5fe0d96f2f24e0d8f6b23d6f
staFF6773 <108166164+staFF6773@users.noreply.github.com>
2024-02-23 13:08:18 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,25 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name= 'image-core.py',
     version= '1.0.0',
     license= 'MIT',
-    long_description=open('README.md').read(),
+    long_description=long_description,
     author= 'SstudiosDev',
     author_email= 'sstudiosdev@gmail.com',
-    url= 'https://github.com/Sstudios-Dev/image-core.py'
+    url= 'https://github.com/Sstudios-Dev/image-core.py',
+    projec_urls={
+        "Bug Tracker": "https://github.com/Sstudios-Dev/image-core.py/issues"
+    },
+    classifiers=[
+        "programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requiere=">=3.6"
 )
