f78a0c294e7109ea24e7fe665e859ac5788d789d
akit <149289426+Sstudiosdev@users.noreply.github.com>
2024-02-23 11:26:03 -0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name= 'image-core.py',
+    version= '1.0.0',
+    license= 'MIT',
+    long_description=open('README.md').read(),
+    author= 'SstudiosDev',
+    author_email= 'sstudiosdev@gmail.com',
+    url= 'https://github.com/Sstudios-Dev/image-core.py'
+)
